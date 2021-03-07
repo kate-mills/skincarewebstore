4478c17a7c942d8c34d0b318cae46cef43d20984
Kate Mills <kate.mills1124@gmail.com>
2021-03-07 15:10:21 -0800

    suspense spinner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+import Spinner from './components/spinner/spinner.component'
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -11,8 +12,10 @@ function App() {
   return (
     <div>
       <Switch>
+        <Suspense fallback={<Spinner/>}>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        </Suspense>
       </Switch>
     </div>
   );
