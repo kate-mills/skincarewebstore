9eb9bc944ff08bca89ff24a95d12c6e352be818b
Kate Mills <kate.mills1124@gmail.com>
2021-03-07 16:30:35 -0800

    Router reconfig

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-//import React, {Suspense} from 'react';
 import React from 'react'
-import { Switch, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
 
 import './App.css';
-
-//import Spinner from './components/spinner/spinner.component';
-//import ErrorBoundary from './components/error-boundary/error-boundary.component';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import ErrorPage from './pages/errorpage/errorpage.component';
+
 import Header from './components/header/header.component'
 
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Switch>
-        {/*<ErrorBoundary>
-           <Suspense fallback={<Spinner />}> */}
-            <Route exact path='/' component={HomePage} />
-            <Route path='/shop' component={ShopPage} />
-            <Route path='*' component={ErrorPage} />
-          {/* </Suspense> </ErrorBoundary>*/}
-      </Switch>
-    </div>
+    <Router>
+      <div>
+        <Header/>
+        <Switch>
+          <Route path="/" exact><HomePage/></Route>
+          <Route path="/shop"><ShopPage/></Route>
+          <Route path="*"><ErrorPage/></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 export default App
+
+
+//import React, {Suspense} from 'react';
+//import Spinner from './components/spinner/spinner.component';
+//import ErrorBoundary from './components/error-boundary/error-boundary.component';
