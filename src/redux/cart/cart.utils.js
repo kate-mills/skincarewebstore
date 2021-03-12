19b15e5c97b5d934bc360bbe40bf884603a06ff9
Kate Mills <kate.mills1124@gmail.com>
2021-03-11 18:09:14 -0800

    increase decrease quantity

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -14,3 +14,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 };
+
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+
+  const existingCartItem = cartItems.find(item => {
+    return item.id === cartItemToRemove.id
+  })
+
+  if(existingCartItem.quantity === 1){
+    return cartItems.filter( cartItem =>  cartItem.id !== cartItemToRemove.id )
+  }
+  else{
+    return cartItems.map( item => {
+      if(item.id === existingCartItem.id){
+        return {quantity:  item.quantity - 1, name: item.name, price: item.price, imageUrl: item.imageUrl, id: item.id }
+      }
+      return (item.id === existingCartItem.id) ? { quantity: item.quantity-1, ...item } : item
+    })
+  }
+};
