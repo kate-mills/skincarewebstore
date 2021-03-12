af478932304343f0463da78f7c732463b4200a1a
Kate Mills <kate.mills1124@gmail.com>
2021-03-11 16:17:12 -0800

    Removed console.log

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -10,7 +10,6 @@ import { createStructuredSelector } from 'reselect'
 import './cart-icon.styles.scss'
 
 const CartIcon  = ({toggleCartHidden, itemCount}) => {
-  console.log(itemCount)
   return (
     <div className='cart-icon' onClick={toggleCartHidden}>
       <ShoppingIcon className='shopping-icon'/>
