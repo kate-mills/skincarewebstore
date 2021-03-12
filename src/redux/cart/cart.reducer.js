7654a3ebbc715555ffce5a7274dce80375dc30d3
Kate Mills <kate.mills1124@gmail.com>
2021-03-11 16:16:25 -0800

    remove cart item

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import { CartActionTypes } from './cart.types'
-import { addItemToCart  } from './cart.utils'
+import { addItemToCart } from './cart.utils'
 
 const INITIAL_STATE = {
   hidden: true,
@@ -13,11 +13,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return { ...state, hidden: !state.hidden, };
 
     case CartActionTypes.ADD_ITEM:
-      return{ ...state, cartItems: addItemToCart(state.cartItems, action.payload) }
+      return { ...state, cartItems: addItemToCart(state.cartItems, action.payload) }
+
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload
+        )
+      };
 
     default:
       return state; 
-
   }
 };
 
